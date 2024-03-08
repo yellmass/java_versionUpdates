e483bb81b34d53dd393a26bc6774b36a5dc4c844
yellmass <ebincyilmaz@gmail.com>
2024-03-07 22:02:12 -0500

    java version updates

--- m6-stream/src/main/java/org/example/tasks/task2.java
+++ m6-stream/src/main/java/org/example/tasks/task2.java
@@ -7,18 +7,17 @@ import java.util.stream.Collectors;
 
 public class task2 {
     public static void main(String[] args) {
-        List<Integer> numbers1 = Arrays.asList(1,2,3);
-        List<Integer> numbers2 = Arrays.asList(3,4);
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(3, 4);
 
         List<int[]> pairs = new ArrayList<>();
 
-                numbers1.stream()
-                .forEach(x->{
-                    numbers2.stream()
-                            .map(y->new int[]{x,y})
-                            .forEach(array->pairs.add(array));
-                });
+        numbers1.stream()
+                .forEach(x -> numbers2.stream()
+                        .map(y -> new int[]{x, y})
+                        .forEach(array -> pairs.add(array))
+                );
 
-        pairs.stream().forEach(p->System.out.println(Arrays.toString(p)));
+        pairs.stream().forEach(p -> System.out.println(Arrays.toString(p)));
     }
 }
